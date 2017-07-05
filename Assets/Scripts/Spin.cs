d58533aba0d1bca58a6fd0da8dc1d81a9670d23f
Eugen-ПК\Eugen <satyagrah108@gmail.com>
2017-07-05 19:21:24 +0300

    Chapter2 paragraphs 2.2 - 2.4.3

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hello : MonoBehaviour {
+public class Spin : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello!");
-	}
+    public float speed = 3.0f;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Rotate(0, speed, 0,Space.Self);
 	}
 }
