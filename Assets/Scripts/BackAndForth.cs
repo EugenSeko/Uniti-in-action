c52dcfd3d3393040d04ea1592463c7d3169a7588
DESKTOP-ERGRU5L\Eugen <Satyagrah108@gmail.com>
2017-08-02 16:03:03 +0300

    4 - 5

---
+++ Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour {
+
+
+    public float speed = 10;
+    public float maxZ = 6;
+    public float minZ = -6;
+    private int _direction=1;
+
+
+    void Update () {
+        transform.Translate(0, 0, speed * Time.deltaTime*_direction);
+        if (transform.position.z >= maxZ || transform.position.z <= minZ)
+        {
+            _direction *= -1;
+            transform.Translate(0, 0, speed * Time.deltaTime * _direction);
+        }
+    }
+}
