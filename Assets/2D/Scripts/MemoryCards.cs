b138bacf311588f6fbfad76a8607481bb4e573d7
DESKTOP-ERGRU5L\Eugen <Satyagrah108@gmail.com>
2017-08-02 21:19:21 +0300

    5 - 5.4 (2D)

---
+++ Assets/2D/Scripts/MemoryCards.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCards : MonoBehaviour {
+
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController1 controller;
+
+    private int _id;
+    public int id
+    {
+        get
+        {
+            return _id;
+        }
+    }
+
+    public void SetCard(int id,Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    } 
+}
---
+++ Assets/2D/Scripts/SceneController1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController1 : MonoBehaviour {
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2.0f;
+    public const float offsetY = 2.5f;
+
+    [SerializeField] private MemoryCards originalCard;
+    [SerializeField] private Sprite[] images;
+
+
+	void Start () {
+        Vector3 startPos = originalCard.transform.position;
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
+
+        for(int i=0; i< gridCols; i++)
+        {
+            for(int j=0; j < gridRows; j++)
+            {
+                MemoryCards card;
+                if(i==0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                  card =  Instantiate(originalCard) as MemoryCards;
+                }
+                // int id = Random.Range(0, images.Length);
+                int index = j * gridCols + i;
+                int id = numbers[index];
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX*i) + startPos.x;
+                float posY = -(offsetY*j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+
+
+        
+	}
+    private int[] ShuffleArray(int[] numbers)  //реализация алгоритма тасования Кнута.
+    {
+        int[]newArray=numbers.Clone()as int[];
+        for(int i = 0; i < newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+        return newArray;
+    }
+	
+}
